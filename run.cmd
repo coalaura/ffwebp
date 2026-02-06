da4e481a164c0f3fa01aa2c514c59746d970f705
Laura <laura@wiese2.org>
2026-02-06 23:17:45 +0100

    rrm old version

--- run.cmd
+++ run.cmd
@@ -1,5 +1,12 @@
 @echo off
 
+set CC=zig cc -target x86_64-windows-gnu
+set CXX=zig c++ -target x86_64-windows-gnu
+
+if exist ffwebp.exe (
+	del ffwebp.exe
+)
+
 echo Rebuilding...
 go build -tags full -o ffwebp.exe .\cmd\ffwebp
 
